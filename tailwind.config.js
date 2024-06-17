214b089bb1a13e87c4ca8770336aae766f451218
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-17 17:34:33 +0700

    chore: Update Tailwind CSS configuration
    This commit updates the Tailwind CSS configuration in the tailwind.config.js file. It extends the theme with additional screen sizes and adds a new background color variant. It also includes a new font family option. This change is necessary to customize the styling of the application and improve the overall user experience.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        xs: '300px'
+      },
       backgroundColor: {
         primary: "#f0f0f0",
-      }
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
     },
   },
   plugins: [require("daisyui")],
