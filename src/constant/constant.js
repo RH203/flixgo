f17f918531690c78e697ac0884fd417c28352cbb
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-19 14:11:46 +0700

    refactor: Update trendingMovieList constant URL in constant.js

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -3,7 +3,7 @@ export const movieGenre = "https://api.themoviedb.org/3/genre/movie/list";
 export const tvGenre = "https://api.themoviedb.org/3/genre/tv/list";
 // List
 export const nowPlayingList = "https://api.themoviedb.org/3/movie/now_playing";
-export const trendingMovieList = "https://api.themoviedb.org/3/trending/movie/day" // + time (day or week)
+export const trendingMovieList = "https://api.themoviedb.org/3/trending/" // + time (day or week)
 export const popularList = "https://api.themoviedb.org/3/movie/popular";
 export const topRatedList = "https://api.themoviedb.org/3/movie/top_rated";
 export const upcomingList = "https://api.themoviedb.org/3/movie/upcoming";
