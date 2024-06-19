8e311cb410c907fb17689543d98f3aa71ca9e9b2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-19 11:48:02 +0700

    feat: Add CardMovie component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import Buttons from "./Buttons/Buttons";
+import CardMovie from "./CardMovie/CardMovie";
 
-export { Navbar, Footer, Buttons };
+export { Navbar, Footer, Buttons, CardMovie };
