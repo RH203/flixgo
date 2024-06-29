b80db16b94b874fca17b3decf5b2db2c597a7f4d
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-29 18:06:59 +0700

    feat: Add PageNotFound component to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import Buttons from "./Buttons/Buttons";
 import CardMovie from "./CardMovie/CardMovie";
+import PageNotFound from "./ErrorPage/PageNotFound.jsx";
 
-export { Navbar, Footer, Buttons, CardMovie };
+export { Navbar, Footer, Buttons, CardMovie, PageNotFound };
