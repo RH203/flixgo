32b2916dc7a799ff8b514eb1356af8259f335343
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-30 15:03:08 +0700

    refactor: Update movie genre URLs in constant.js
    Update the movie genre URLs in the constant.js file to use more descriptive variable names. This change improves code readability and maintainability.

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,12 +1,18 @@
 // Category
-export const movieGenre = "https://api.themoviedb.org/3/genre/movie/list";
+export const movieGenreUrl = "https://api.themoviedb.org/3/genre/movie/list";
 export const tvGenre = "https://api.themoviedb.org/3/genre/tv/list";
+
 // List
 export const nowPlayingList = "https://api.themoviedb.org/3/movie/now_playing";
-export const trendingMovieList = "https://api.themoviedb.org/3/trending/" // + category + time (day or week)
+export const trendingMovieListUrl = "https://api.themoviedb.org/3/trending/" // + category + time (day or week)
 export const popularList = "https://api.themoviedb.org/3/movie/popular";
 export const topRatedList = "https://api.themoviedb.org/3/movie/top_rated";
 export const upcomingList = "https://api.themoviedb.org/3/movie/upcoming";
+
 //! For pagination = https://api.themoviedb.org/3/movie/popular?page=2
+
 // Image
 export const imageMovie = "https://image.tmdb.org/t/p/w185/" // + path image (/xxxxx.jpg)
+
+// Tv series
+export const tvSeriesUrl = "https://api.themoviedb.org/3/tv/" // airing_today, on_the_air, popular, top_rated
