33664e06dae5111cb76dda58adc2692f574443a3
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-03 15:27:06 +0700

    refactor: Remove unused code and optimize movieSlice.js

--- src/redux/slice/movieSlice.js
+++ src/redux/slice/movieSlice.js
@@ -2,7 +2,6 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
   data: [],
-  init: false
 }
 
 const movieSlice = createSlice({
@@ -10,25 +9,13 @@ const movieSlice = createSlice({
   initialState,
   reducers: {
     setMovie: (state, action) => {
-      if (state.init) {
-        state.data = action.payload;
-      } else if (state.data.length === 0) {
-        state.data = action.payload;
-      } else {
-        swapData(state.data, action.payload);
-      }
-    },
-    setInitLoad: (state, action) => {
-      state.init = action.payload;
+      state.data = action.payload;
     }
-  }
+  },
+
 })
 
-function swapData(data, newData) {
-  for (let i = 0; i < data.length; i++) {
-    data[i] = newData[i]
-  }
-}
 
-export const {setMovie, setInitLoad} = movieSlice.actions;
+
+export const {setMovie} = movieSlice.actions;
 export default movieSlice.reducer
