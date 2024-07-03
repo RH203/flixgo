cc17e66419586333c0e272ae41a9276b1e1b5502
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-03 15:27:00 +0700

    refactor: Add cartSlice to manage cart data in Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import categoryMovieReducer from "./slice/categoryMovieSlice";
 import movieSliceReducer from "./slice/movieSlice.js"
 import homepageSliceReducer from "./slice/homepageSlice.js"
+import cartSliceReducer from "./slice/cartSlice.js"
 
 export const store = configureStore({
   reducer: {
     categoryMovie: categoryMovieReducer,
     movie: movieSliceReducer,
-    homepage: homepageSliceReducer
+    homepage: homepageSliceReducer,
+    cart: cartSliceReducer
   },
 });
