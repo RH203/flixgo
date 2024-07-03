ce37ab8f02c04a35ca4c1112a9de73d46d03e766
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-03 15:26:45 +0700

    refactor: Add lazy loading for images in DetailMovie component
    This commit adds lazy loading for images in the DetailMovie component, improving the page load time by loading images only when they are visible on the screen. This optimization enhances the performance and user experience of the application.

--- src/pages/DetailMovie/DetailMovie.jsx
+++ src/pages/DetailMovie/DetailMovie.jsx
@@ -1,29 +1,107 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {imageMovie} from "../../constant/constant.js";
+import {hasFlag} from "country-flag-icons";
+import {convertIso} from "../../controller/controller.js";
+import {increment} from "../../redux/slice/cartSlice.js";
 
 function DetailMovie() {
+  const dispatch = useDispatch();
   const {id} = useParams();
+
+  const favorite = useSelector((state) => state.cart.total)
   const data = useSelector((state) => state.movie.data);
+  const popularData = useSelector((state) => state.homepage.dataPopular)
+  const tvSeries = useSelector((state) => state.homepage.dataTvSeries)
+  const genres = useSelector((state) => state.categoryMovie.genres) || [];
+
   const [detailMovie, setDetailMovie] = useState(null);
 
+
   useEffect(() => {
-    const findMovie = (id) => {
-      const movie = data.find((movie) => movie.id === parseInt(id));
+    const findMovie = () => {
+      let movie = data.find((movie) => movie.id === parseInt(id));
+      if (!movie && popularData) {
+        movie = popularData.find((movie) => movie.id === parseInt(id));
+      }
+      if (!movie && tvSeries) {
+        movie = tvSeries.find((movie) => movie.id === parseInt(id));
+      }
       setDetailMovie(movie || null);
     };
-    if (data.length > 0) {
-      findMovie(id);
+
+    if (data.length > 0 || popularData || tvSeries) {
+      findMovie();
     }
-  }, [id, data]);
+  }, [id, data, popularData, tvSeries]);
+
+  const renderGenres = (genreIds) => {
+    if (!genreIds) return null;
+    return genreIds
+      .map((id) => genres.find((genre) => genre.id === id))
+      .filter((genre) => genre !== undefined)
+      .map((genre, index) => (
+        <span className={"bg-indigo-400 py-1 rounded-lg text-sm text-center"} key={index}>
+          {`${genre.name}`}
+        </span>
+      ));
+  };
+
+  const addToCart = () => {
+    dispatch(increment())
+  }
 
   return (
-    <div>
+    <div className={"w-full"}>
       {detailMovie !== null ? (
-        <p>{detailMovie.title}</p>
+        <div className={"grid grid-cols-3 gap-3"}>
+          <img src={imageMovie + (detailMovie.poster_path || detailMovie.profile_path)}
+               alt={detailMovie.title || detailMovie.name} className={"col-end-1 rounded-lg"}/>
+
+          <div className={"col-span-2 flex flex-col justify-between"}>
+
+            <div>
+              <p className={"font-medium text-xl flex items-center gap-3 "}>
+                {detailMovie.title || detailMovie.name}
+                <div>
+                  {hasFlag(convertIso(detailMovie.original_language)) ? (
+                    <img
+                      alt="Country Flag"
+                      src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${convertIso(detailMovie.original_language)}.svg`}
+                      className={"size-5"}
+                    />
+                  ) : (
+                    <p>No icon</p>
+                  )}
+                </div>
+              </p>
+              <p className={"text-sm"}>{detailMovie.release_date}</p>
+              <p className={"text-sm mt-5"}>{detailMovie.overview}</p>
+            </div>
+
+
+            <div className={"flex items-center justify-between"}>
+              <div className={"grid grid-cols-2 w-1/2 gap-2"}>
+                {renderGenres(detailMovie.genre_ids)}
+              </div>
+              <div className="radial-progress text-indigo-400 "
+                   style={{"--value": (detailMovie.vote_average * 1000) / 100}}
+                   role="progressbar">
+                {(detailMovie.vote_average * 1000) / 100}%
+              </div>
+            </div>
+          </div>
+
+        </div>
       ) : (
         <p>Data not found</p>
       )}
+      <button type="submit" className={"btn bg-indigo-600 text-white hover:bg-indigo-400 mt-10"} onClick={addToCart}>Add
+        to
+        favorite
+      </button>
+
     </div>
   );
 }
