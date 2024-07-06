afdcff1232f106ade4304220c133c3cfe4ccdf4f
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-06 19:36:12 +0700

    refactor: Add detailSliceReducer to store
    This commit adds the detailSliceReducer to the store configuration in the src/redux/store.js file. The detailSliceReducer is responsible for managing the state related to movie details. This addition enhances the functionality of the application by providing a centralized store for movie details.

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import categoryMovieReducer from "./slice/categoryMovieSlice";
 import movieSliceReducer from "./slice/movieSlice.js"
 import homepageSliceReducer from "./slice/homepageSlice.js"
 import cartSliceReducer from "./slice/cartSlice.js"
+import detailSliceReducer from "./slice/detailSlice.js"
 
 export const store = configureStore({
   reducer: {
     categoryMovie: categoryMovieReducer,
     movie: movieSliceReducer,
     homepage: homepageSliceReducer,
-    cart: cartSliceReducer
+    cart: cartSliceReducer,
+    detail: detailSliceReducer
   },
 });
